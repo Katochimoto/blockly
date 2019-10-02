c1006b6b82a752ca45542dc31f6617a38006a365
Anton Tursenev <anton@rikishi.info>
2019-10-02 08:57:15 +0700

    Initial commit

---
+++ rollup.config.js
@@ -0,0 +1,53 @@
+import path from 'path'
+import commonjs from 'rollup-plugin-commonjs'
+import resolve from 'rollup-plugin-node-resolve'
+import postcss from 'rollup-plugin-postcss'
+import builtins from 'builtin-modules'
+import postcssCopy from 'postcss-copy'
+
+export default [
+  {
+    input: 'src/index.js',
+
+    external: builtins,
+
+    context: 'window',
+
+    plugins: [
+      postcss({
+        extract: path.resolve('lib/index.css'),
+        extensions: [ '.css' ],
+        modules: {
+          scopeBehaviour: 'global',
+          camelCase: true,
+          generateScopedName: '[name]__[local]___[hash:base64:5]',
+        },
+        plugins: [
+          postcssCopy({
+            basePath: [
+              path.resolve('node_modules/blockly')
+            ],
+            dest: path.resolve('lib')
+          }),
+        ],
+      }),
+
+      resolve(),
+      commonjs(),
+    ],
+
+    output: {
+      file: 'lib/index.js',
+      format: 'umd',
+      name: 'Blockly',
+    },
+
+    onwarn (warning, warn) {
+      if (warning.code === 'EVAL') {
+        return
+      }
+
+      warn(warning)
+    }
+  }
+]
