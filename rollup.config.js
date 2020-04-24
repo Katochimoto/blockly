0e8df400ce35a9c428932ae09e21e6909ba9c176
Anton Tursenev <anton@rikishi.info>
2020-04-24 12:26:25 +0700

    Update dep

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,10 @@
 import path from 'path'
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
-// import json from '@rollup/plugin-json'
 import postcss from 'rollup-plugin-postcss'
 import analyze from 'rollup-plugin-analyzer'
-// import { terser } from 'rollup-plugin-terser'
 import builtins from 'builtin-modules'
 import postcssCopy from 'postcss-copy'
-// import builtins from 'rollup-plugin-node-builtins'
-// import globals from 'rollup-plugin-node-globals'
 
 export default [
   {
@@ -18,9 +14,6 @@ export default [
     context: 'window',
 
     plugins: [
-      // globals(),
-      // builtins(),
-      // json({ compact: true, preferConst: true }),
       postcss({
         extract: path.resolve('lib/index.css'),
         extensions: [ '.css' ],
@@ -41,15 +34,10 @@ export default [
 
       resolve(),
       commonjs(),
-      // terser(),
       analyze({ limit: 5 }),
     ],
 
     output: [
-      // {
-      //   file: 'lib/index.js',
-      //   format: 'es',
-      // },
       {
         file: 'lib/index.js',
         format: 'umd',
