bed7c5a1f67d9379ded245aeca319122e8abc0b6
Anton Tursenev <anton@rikishi.info>
2020-04-24 11:59:35 +0700

    Rebuild

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,27 @@
 import path from 'path'
-import commonjs from 'rollup-plugin-commonjs'
-import resolve from 'rollup-plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import json from '@rollup/plugin-json'
 import postcss from 'rollup-plugin-postcss'
-import builtins from 'builtin-modules'
+import analyze from 'rollup-plugin-analyzer'
+import { terser } from 'rollup-plugin-terser'
+// import builtins from 'builtin-modules'
 import postcssCopy from 'postcss-copy'
+import builtins from 'rollup-plugin-node-builtins'
+import globals from 'rollup-plugin-node-globals'
 
 export default [
   {
     input: 'src/index.js',
 
-    external: builtins,
-
-    context: 'window',
+    treeshake: true,
+    // external: builtins,
+    // context: 'window',
 
     plugins: [
+      // globals(),
+      builtins(),
+      json({ compact: true, preferConst: true }),
       postcss({
         extract: path.resolve('lib/index.css'),
         extensions: [ '.css' ],
@@ -34,13 +42,21 @@ export default [
 
       resolve(),
       commonjs(),
+      terser(),
+      analyze({ limit: 5 }),
     ],
 
-    output: {
-      file: 'lib/index.js',
-      format: 'umd',
-      name: 'Blockly',
-    },
+    output: [
+      {
+        file: 'lib/index.js',
+        format: 'es',
+      },
+      {
+        file: 'lib/index.umd.js',
+        format: 'umd',
+        name: 'Blockly',
+      },
+    ],
 
     onwarn (warning, warn) {
       if (warning.code === 'EVAL') {
